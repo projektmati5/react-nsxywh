f29209ec3b56968e49f4f2640476115748f840fd
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-12-17 20:16:47 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,88 +3,47 @@ import "./style.css";
 import 'semantic-ui-css/semantic.min.css'
 
 
-function Przycisk(){
-  return(<button onClick={Komunikat}>Kliknij!</button>)
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {counter: 0, drugiCounter: 0, trzeciCounter: 0};
 
-function Komunikat(){
-  return(alert("Klik"))
-}
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment.bind(this)}>+</button>
 
-
-function AppHeader() {
-  return (
-    <header className="ui menu">
-      <nav className="ui container">
-        <a href="#" className="header item">
-          <img
-            className="logo"
-            src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png"
-          />
-          Lista kontaktów
-        </a>
-        <div className="header item">
-          <button className="ui button">Dodaj</button>
-        </div>
-      </nav>
-    </header>
-  );
-}
-
-function ContactsList() {
-  return (
-console.log("a"),
-    
-
-    <ul className="ui relaxed divided list selection">
-    
-      <ContactItem
-        login="typeofweb1"
-        name="Lena"
-        department="JavaScript Developer"
-      />
-      <ContactItem
-        login="typeofweb2"
-        name="Brian"
-        department="Human Resources"
-      />
-      <ContactItem
-        login="typeofweb3"
-        name="Rick"
-        department="QA"
-      />
-<ContactItem
-        login="typeofweb3"
-        name="Rick"
-        department="QA"
-      />
-    </ul>
-  );
-}
-
-function ContactItem({ login, name, department }) {
-  const imgUrl = "https://upload.wikimedia.org/wikipedia/en/1/12/Flag_of_Poland.svg";
-  return (
-    <li className="item">
-      <img src={imgUrl} className="ui mini rounded image" />
-      <div className="content">
-        <h4 className="header">{name}</h4>
-        <div className="description">{department}</div>
+        <div><output>{this.state.counter}</output></div>
+       <div> <output>{this.state.drugiCounter}</output></div>
+       <div onClick={this.wynikIncrement.bind(this)}> <output>{this.state.trzeciCounter}</output></div>
+        <button onClick={this.decrement.bind(this)}>-</button>
+        
       </div>
-    </li>
-  );
+    );
+  }
+  
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1,
+      drugiCounter: this.state.drugiCounter + 1
+    })
+  }
+  
+wynikIncrement(){
+  this.setState({trzeciCounter:this.state.trzeciCounter+1})
 }
 
-export default function App() {
-  return (
-    <div>
-      <AppHeader />
-      <Przycisk/>
-      <main className="ui main text container">
-        <ContactsList />
-      </main>
-    </div>
-  );
+
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1,
+      drugiCounter: this.state.drugiCounter + 1
+    })
+  }
 }
+export default App
+//ReactDOM.render(<App />, document.getElementById("app"));
 
 //ReactDOM.render(<App />, document.getElementById("app"));
